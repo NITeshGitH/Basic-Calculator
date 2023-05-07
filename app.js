9cd951f75a82cdabac52bbd6d709f5eda97a8e23
Nitesh Verma <117501549+NITeshGitH@users.noreply.github.com>
2023-05-07 11:22:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const outputEl = document.querySelector(".text");
+const valEl = document.querySelectorAll(".val");
+const ClearEl = document.querySelector(".AC");
+const deleteEl = document.querySelector(".DEL");
+const equalEl = document.querySelector(".equal");
+
+// It adds every value and display at the output
+valEl.forEach((val)=>{
+    val.addEventListener("click",()=>{
+        outputEl.value += val.value ;
+    })
+})
+ClearEl.addEventListener("click",()=>{
+    outputEl.value = "";
+})
+
+deleteEl.addEventListener("click",()=>{
+    outputEl.value = outputEl.value.slice(0,-1);
+})
+
+equalEl.addEventListener("click",()=>{
+    try{
+        outputEl.value = eval(outputEl.value);
+    }catch(e){
+        alert(e);
+    }
+})
